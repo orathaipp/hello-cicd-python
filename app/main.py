1a823373d82ea157fa87661eb287f45b281b8473
Chalunthorn <chalunthorn.kandamri@gmail.com>
2025-06-25 15:06:10 +0700

    test: test

--- app/main.py
+++ app/main.py
@@ -12,3 +12,7 @@ def hello():
     pod_name = os.environ.get("HOSTNAME", "unknown")
     logging.info(f"Responding from pod: {pod_name}")
     return {"message": "Hello CI/CD V2 🚀", "pod_name": pod_name}
+
+@app.get("/test")
+def test():
+    return {"Test, Hello World!"}
