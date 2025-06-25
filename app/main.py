6f6fb0a7dc1981cec9596570629a6f567273326a
orathaipp <orathai.k@v-xip.com>
2025-06-25 14:27:14 +0700

    update: log pod name in response

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import os
+import logging
 
 app = FastAPI()
 
+# ตั้งค่า logging
+logging.basicConfig(level=logging.INFO)
+
 @app.get("/")
 def hello():
-    return {"message": "Hello CI/CD V2 🚀"} 
+    pod_name = os.environ.get("HOSTNAME", "unknown")
+    logging.info(f"Responding from pod: {pod_name}")
+    return {"message": "Hello CI/CD V2 🚀", "pod_name": pod_name}
